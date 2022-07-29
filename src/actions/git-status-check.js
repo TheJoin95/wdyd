ee6e30060c8a73c646bd63f3e8d4f4671d0c8845
Miki <miki.lombi@gmail.com>
2022-07-29 14:24:10 +0200

    feat(check-git-add): add check on git status

---
+++ src/actions/git-status-check.js
@@ -0,0 +1,24 @@
+const { exec } = require("child_process")
+var clc = require("cli-color")
+
+const error = clc.red.bold
+const notice = clc.blue
+const success = clc.green
+
+module.exports = () => {
+    exec(`git status --porcelain | grep ^[AMDR] | awk '{ print $2 }'`, (err, stdout) => {
+        if (err) {
+            console.error(error(err))
+            return
+        }
+
+        if (stdout.trim() === '') {
+            console.log(error('Nothing to commit, please check again and run at least \'git add <filename>\'.\nExiting'))
+            process.exit()
+        } else {
+            console.log(notice('You are commiting the following files:'))
+            console.log(success(stdout))
+        }
+
+    })
+}
